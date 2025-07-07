39de752cb2854f022cfabaf4a07afc9557054505
vishal17 <k26200017@gmail.com>
2025-07-07 21:25:03 +0530

    command line arguments and environment variable examples

---
+++ Day-05/ENV_example.py
@@ -0,0 +1,4 @@
+import os
+
+my_name = os.environ.get('MY_NAME')
+print("Hello,", my_name)
---
+++ Day-05/command_line_arguments_example.py
@@ -0,0 +1,9 @@
+#Adding Two Numbers from Command Line.
+
+import sys
+
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
+
+result = num1+num2
+print("The sum is:", result)
